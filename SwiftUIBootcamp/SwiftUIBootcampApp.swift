a3c2b316f16bc18a8cfc7f280d3aebfe52bf1a29
Matthhew Pters <mattsterp@gmail.com>
2021-07-05 12:31:19 -0400

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Matthew Peters on 7/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
